5e80e02efadd8d2eb7d30a08e98345ba3aa65ac6
wyzxcv <wy3021058459@outlook.com>
2026-03-08 14:46:27 +0800

    Site updated: 2026-03-08 14:46:26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1772952189491";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1772952382660";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
