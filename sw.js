cbaee124d863a3976d8bc6ff65a6c7e3d584aac1
wyzxcv <wyzxcv@users.noreply.github.com>
2026-08-28 05:14:11 +0000

    deploy: wyzxcv/WYBlog@fe80ef3c0aa06efc5b94f3a1618c8d61540bbecb

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1787482448875";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1787894046339";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
